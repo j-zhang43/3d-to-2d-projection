11c51d388545f8063dfdbd589b2aa964fae52a11
Jason Zhang <jaszhang43@gmail.com>
2026-02-01 01:39:27 -0500

    finished 3d-2d projection, next is rotation animation

--- projection.cpp
+++ projection.cpp
@@ -19,6 +19,35 @@ struct Point3D {
 
 class Graph {
 public:
+    void process_input() {
+        string junk;
+        cin >> junk >> MIN_X >> 
+               junk >> MAX_X >>
+               junk >> MIN_Y >>
+               junk >> MAX_Y;
+        size_t size;
+        cin >> size;
+        points3d.resize(size);
+        points2d.reserve(size);
+        for (size_t i = 0; i<size; ++i) {
+            Point3D p;
+            cin >> p.x >> p.y >> p.z;
+            points3d[i] = p;
+        }
+        cin >> size;
+        edges.resize(size);
+        for (size_t i = 0; i<size; ++i) {
+            size_t idx_1, idx_2;
+            cin >> idx_1 >> idx_2;
+            edges[i] = {idx_1, idx_2};
+        }
+
+        size_x = static_cast<size_t>(MAX_X+1-MIN_X);
+        size_y = static_cast<size_t>(MAX_Y+1-MIN_Y);
+
+        graph = vector<vector<char>>(size_x, vector<char>(size_y, ' '));
+    }
+
     char get_char(double angle) {
         char angle_char;  
         if (angle < -150) {
@@ -42,14 +71,16 @@ public:
         }
         return angle_char;
     }
+
     void update_graph() {
+        convert_dim();
+        
         for (const auto &edge: edges) {
             Point2D &p1 = points2d[edge.first];
             Point2D &p2 = points2d[edge.second];
 
             double angle = atan2(p2.y-p1.y, p2.x - p1.x) * (180.0/M_PI);
             double dist = sqrt(pow(p2.x-p1.x,2) + pow(p2.y-p1.y,2));
-            cout << edge.first << " " << edge.second << " "<< angle << '\n';
             char angle_char = get_char(angle);
             double cur_x = p1.x;
             double cur_y = p1.y;
@@ -69,6 +100,7 @@ public:
             graph[x][y] = 'o';
         }
     }
+
     void draw_graph() {
         for (size_t i = 0; i < size_x+2; ++i) {
             cout << '-';
@@ -88,24 +120,35 @@ public:
         cout << '\n';
     }
     
+    void convert_dim() {
+        points2d.clear();
+
+        for (Point3D &p3d: points3d) {
+            Point2D p2d;
+            p2d.x = 0.5*p3d.x + 1*p3d.y;
+            p2d.y = 0.5*p3d.x + 1*p3d.z;
+            points2d.push_back(p2d);
+        }
+    }
 
 private: 
-    int MIN_X = -10;
-    int MAX_X = 10;
-    int MIN_Y = -10;
-    int MAX_Y = 10;
+    int MIN_X;
+    int MAX_X;
+    int MIN_Y;
+    int MAX_Y;
 
-    size_t size_x = static_cast<size_t>(MAX_X+1-MIN_X);
-    size_t size_y = static_cast<size_t>(MAX_Y+1-MIN_Y);
+    size_t size_x;
+    size_t size_y;
 
-    vector<vector<char>> graph = vector<vector<char>>(size_x, vector<char>(size_y, ' '));
-    vector<Point3D> points3d = {{0,0,0}, {5,0,0}, {0,5,0}};
-    vector<Point2D> points2d = {{0,0}, {-5,10}, {0, 10}};
-    vector<pair<size_t, size_t>> edges = {{0,1}, {0,2}, {1,2}};
+    vector<vector<char>> graph;
+    vector<Point3D> points3d;
+    vector<Point2D> points2d;
+    vector<pair<size_t, size_t>> edges;
 };
 
 int main () {
     Graph g = Graph();
+    g.process_input();
     g.update_graph();
     g.draw_graph();
 }
